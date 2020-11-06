a76833fb75245567fc2c62b09662c07d34f0af08
potof <slptmac@gmail.com>
2020-11-07 04:45:19 +0900

    独力ではたどり着かなかった・・・

--- src/ABC181_C.py
+++ src/ABC181_C.py
@@ -1,17 +1,19 @@
 
 n = int(input())
-xy = [list(map(int, input().split())) for _ in range(n)]
+xy = [tuple(map(int, input().split())) for _ in range(n)]
 
-x = [i[0] for i in xy]
-xc = [x.count(i) for i in x]
-y = [i[1] for i in xy]
-yc = [y.count(i) for i in y]
-if max(xc) >= 3 or max(yc) >= 3:
-    print("Yes")
-else:
-    k = [i[1] - i[0] for i in xy]
-    kc = [k.count(i) for i in k]
-    if max(kc) >= 3:
-        print("Yes")
-    else:
-        print("No")
+for i in range(n):
+    for j in range(i):
+        for k in range(j):
+            x1, y1 = xy[i]
+            x2, y2 = xy[j]
+            x3, y3 = xy[k]
+            x1 -= x3
+            x2 -= x3
+            y1 -= y3
+            y2 -= y3
+            if x1 * y2 == x2 * y1:
+                print("Yes")
+                exit()
+
+print("No")

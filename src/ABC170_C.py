a563e7c3950c9fdb0920e20df07a105d0ac6dffc
potof <slptmac@gmail.com>
2020-11-27 01:21:36 +0900

    回答を見て書き直してみた

--- src/ABC170_C.py
+++ src/ABC170_C.py
@@ -2,20 +2,10 @@
 x, n = map(int, input().split())
 p_list = list(map(int, input().split()))
 
-a1 = 101
-b1 = 101
-
-for i in range(x-1, -1, -1):
-    if i not in p_list:
-        a1 = i
-        break
-
-for i in range(x, 101):
-    if i not in p_list:
-        b1 = i
-        break
-
-if abs(x-a1) > abs(x-b1):
-    print(b1)
-else:
-    print(a1)
+for i in range(x+1):
+    # マイナスからやらないとだめ（同じ場合は小さい方を回答するから）
+    for j in [-1, 1]:
+        ans = x + i*j
+        if ans not in p_list:
+            print(ans)
+            exit()
